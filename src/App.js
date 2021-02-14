1598828d72c8d32f62d0f6048b609756f9b48172
Thrashskin <jmontegonz@gmail.com>
2021-02-14 14:26:44 +0100

    Initial commit client side

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Signup from './components/auth/Signup'
+import {Link, Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Matched - The first step to find the special one ;) (We are talking about a job!)
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Link to='/signup'>Signup</Link>
       </header>
+      <Switch>
+        <Route exact path="/signup" component={Signup}/>
+      </Switch>
     </div>
   );
 }
---
+++ src/components/auth/Signup.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+export default class Signup extends Component {
+
+    render(){
+        return(
+          <div>
+              signup form
+            {/* <form onSubmit={this.handleFormSubmit}>
+              <label>Email:</label>
+              <input type="text" name="email" value={this.state.email} onChange={ e => this.handleChange(e)}/>
+              
+              <label>Password:</label>
+              <input name="password" value={this.state.password} onChange={ e => this.handleChange(e)} />
+              
+              <input type="submit" value="Signup" />
+            </form>
+       
+            <p>Already have account? 
+                <Link to={"/"}> Login</Link>
+            </p>
+        */}
+          </div>
+        )
+      }
+}
