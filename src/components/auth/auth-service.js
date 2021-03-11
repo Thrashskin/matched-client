f8e8248acdc4bd302898355abd1e91a649525361
Thrashskin <jmontegonz@gmail.com>
2021-03-11 21:52:45 +0100

    Changed API_URL to REACT_APP_API_URL

--- src/components/auth/auth-service.js
+++ src/components/auth/auth-service.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 class AuthService {
     constructor() {
         let service = axios.create({
-            baseURL: `${process.env.API_URL}/api`,
+            baseURL: `${process.env.REACT_APP_API_URL}/api`,
             withCredentials: true
         });
 
