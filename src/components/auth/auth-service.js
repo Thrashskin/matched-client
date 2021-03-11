f883817814385e28b58fd685d386299b60aa7385
Thrashskin <jmontegonz@gmail.com>
2021-03-11 20:50:46 +0100

    base url changed

--- src/components/auth/auth-service.js
+++ src/components/auth/auth-service.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 class AuthService {
     constructor() {
         let service = axios.create({
-            baseURL: 'https://match3d.herokuapp.com',
+            baseURL: 'https://match3d.herokuapp.com/api',
             withCredentials: true
         });
 
